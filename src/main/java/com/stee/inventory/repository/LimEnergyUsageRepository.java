b34e1e1c04435ed6eea78bbbdd54b5e013994c6e
yangjing <851636041@qq.com>
2018-03-01 15:47:30 +0800

    delete

--- src/main/java/com/stee/inventory/repository/LimEnergyUsageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stee.inventory.repository;
-
-import com.stee.sel.lim.reading.LiminaireEnergyHist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface LimEnergyUsageRepository extends JpaRepository<LiminaireEnergyHist, String>, JpaSpecificationExecutor<LiminaireEnergyHist>{
-
-}
