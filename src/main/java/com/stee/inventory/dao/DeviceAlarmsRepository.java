4aab2844d04a94177f3fcd3da902b96068961b31
yangjing <851636041@qq.com>
2018-03-13 13:48:15 +0800

    调试后提交

---
+++ src/main/java/com/stee/inventory/dao/DeviceAlarmsRepository.java
@@ -0,0 +1,15 @@
+package com.stee.inventory.dao;
+
+import com.stee.inventory.entity.sel.DeviceAlarmsEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by SerryMiano on 2017/1/10.
+ */
+public interface DeviceAlarmsRepository extends JpaRepository<DeviceAlarmsEntity, String> {
+    List<DeviceAlarmsEntity> findBySource(Pageable pageable, String source);
+}
