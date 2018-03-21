e8ee86af074b254bbb18212b03dbc4488c1dbce8
yangjing <851636041@qq.com>
2018-03-21 10:32:59 +0800

    添加新的方法

--- src/main/java/com/stee/inventory/dao/LampPoleDao.java
+++ src/main/java/com/stee/inventory/dao/LampPoleDao.java
@@ -1,16 +1,15 @@
 package com.stee.inventory.dao;
 
-import com.stee.inventory.entity.LampPole;
-import com.stee.inventory.entity.LampPoleModel;
-import com.stee.inventory.entity.sel.LampPoleEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.stee.inventory.entity.sel.LampPoleEntity;
 
 public interface LampPoleDao extends JpaRepository<LampPoleEntity,String>{
         LampPoleEntity findByLampPoleId(String id);
+        @Query("select t from LampPoleEntity t where t.lampPoleId in(:poleIds)")
+        List<LampPoleEntity> findByPoleList(@Param(value = "poleIds")List<String> poleIds);
 }
