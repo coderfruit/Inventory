01452e52a0cdea30e2229fe3b12afad1899f5937
yangjing <851636041@qq.com>
2018-03-26 09:38:53 +0800

    释放注释掉的代码

--- src/main/java/com/stee/inventory/controller/LampPoleController.java
+++ src/main/java/com/stee/inventory/controller/LampPoleController.java
@@ -1,10 +1,14 @@
 package com.stee.inventory.controller;
 
 import com.stee.inventory.dao.LampPoleDao;
+import com.stee.inventory.entity.Result;
+import com.stee.sel.inventory.LampPoleEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * lamp model configuration
@@ -17,11 +21,11 @@ public class LampPoleController {
     @Resource
     private LampPoleDao lampPoleDao;
 
-//    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
-//    public Result<LampPoleModel> getAll(String id) {
-//        LampPole lampPole=lampPoleDao.findByLampPoleId(id);
-//        return new Result().success(lampPole);
-//    }
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public Result getAll() {
+        List<LampPoleEntity> lampPoles=lampPoleDao.findAll();
+        return new Result().success(lampPoles);
+    }
 
 
 }
