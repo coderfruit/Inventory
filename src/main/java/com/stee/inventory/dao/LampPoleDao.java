b63444a4eef06acb956c99cbd38c6dc5d13eec31
yangjing <851636041@qq.com>
2018-03-26 09:37:13 +0800

    新增delete方法

--- src/main/java/com/stee/inventory/dao/LampPoleDao.java
+++ src/main/java/com/stee/inventory/dao/LampPoleDao.java
@@ -11,4 +11,6 @@ public interface LampPoleDao extends JpaRepository<LampPoleEntity,String>{
         LampPoleEntity findByLampPoleId(String id);
         @Query("select t from LampPoleEntity t where t.lampPoleId in(:poleIds)")
         List<LampPoleEntity> findByPoleList(@Param(value = "poleIds")List<String> poleIds);
+
+        void deleteByLampPoleModelId(String modelId);
 }
